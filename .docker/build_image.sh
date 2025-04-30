59939d4e19212b70d49f119f30f3832ab396cdad
Prottay-Roy-Chowdhury <ar.prottay@outlook.com>
2025-04-30 17:21:55 +0200

    .docker 2nd edit

--- .docker/build_image.sh
+++ .docker/build_image.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo -e "Building Docker image: UR_easy_handeye2:latest"
+echo -e "Building Docker image: ur_easy_handeye2:latest"
 
 DOCKER_BUILDKIT=1 \
 docker build --pull --rm \
     -f .docker/Dockerfile \
     --build-arg BUILDKIT_INLINE_CACHE=1 \
-    --tag UR_easy_handeye2:latest .
+    --tag ur_easy_handeye2:latest .
